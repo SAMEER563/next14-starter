9b75d5fc7afd6869b4c1a6d67ac0d78dcdbf055e
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-02-03 16:32:11 +0530

    footer done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,11 @@
-
+import styles from "./footer.module.css"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>༒₰iᴍ℘le 多oץ ༒</div>
+      <div className={styles.text}> SAM creative thoughts agency ⓒ All right reserved. </div>
+    </div>
   )
 }
 
