e776a2b22331377e9191664c1e72b6773d07f6de
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-02-06 19:19:06 +0530

    mongo db connection

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -27,35 +27,29 @@ const SinglePostPage = async ({params}) => {
   console.log(post)
   return (
     <div className={styles.container}>
-      <div className={styles.imgContainer}>
+     {post.img &&  <div className={styles.imgContainer}>
         <Image
-          src="https://images.pexels.com/photos/13398438/pexels-photo-13398438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          src={post.img}
           alt=""
           fill
           className={styles.img}
         />
-      </div>
+      </div>}
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>{post?.title}</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
-          <Image
-            className={styles.avatar}
-            src="https://images.pexels.com/photos/13398438/pexels-photo-13398438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            width={50}
-            height={50}
-          />
+          
           {post && (<Suspense fallback={<div>Loading...</div>}>
           <PostUser userId = {post.userId}/>
           </Suspense>
           )}
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
-            <span className={styles.detailValue}>01.02.2024</span>
+            <span className={styles.detailValue}>{post.createdAt.toString().slice(4,16)}</span>
           </div>
         </div>
         <div className={styles.content}>
-        {post?.body}
+        {post.desc}
         </div>
       </div>
     </div>
